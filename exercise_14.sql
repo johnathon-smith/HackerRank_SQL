7276f76be72c9228da6f8291bf17652425c1e060
Johnathon Smith <smith.johnathon01@gmail.com>
2021-09-04 15:22:59 -0500

    Added HackerRank SQL exercise 14

---
+++ exercise_14.sql
@@ -0,0 +1,14 @@
+--Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+--The STATION table is described as follows:
+
+--           FIELD  |   TYPE
+--          -----------------
+--           ID     |   NUMBER
+--           CITY   |   VARCHAR2(21)
+--          STATE   |   VARCHAR2(2)
+--          LAT_N   |   NUMBER
+--          LONG_W  |   NUMBER
+
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY REGEXP '^[^AEIOU]';
